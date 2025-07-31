cb5de1dd04fdb81d00ee006d78824ba45ae9388f
jeff20501 <jeffkimmuna@gmail.com>
2025-07-31 21:27:50 +0300

    .

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './Hang_Man/style.css'
-import { Main } from './Hang_Man/Main'
+import './HangMan/style.css'
+import { Main } from './HangMan/Main'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Main />
